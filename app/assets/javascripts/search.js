aec1ac75e4fd68dd0edfb9c47b194cd05aaff861
Yoshitaka8419 <yoshitakakuwano@gmail.com>
2019-01-22 16:25:25 +0900

    Chatspaceインクリメンタルサーチ実装 修正1

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -14,7 +14,7 @@ $(function(){
   }
 
   function appendMembers(name, user_id) {
-    var html = `<div class='chat-group-user clearfix js-chat-member' id='chat_group_user_22'>
+    var html = `<div class='chat-group-user clearfix js-chat-member'>
                 <input name='group[user_ids][]' type='hidden' value="${user_id}">
                 <p class='chat-group-user__name'>${name}</p>
                 <a class='user_search_remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
@@ -23,22 +23,30 @@ $(function(){
     member_list.append(html);
   }
 
+
+  var user_ids = [];
   $(function() {
     $(".chat-group-form__input").on("keyup", function() {
       var input = $("#user-search-field").val();
+      console.log(user_ids)
+
       $.ajax({
         type: 'GET',
         url: '/users',
-        data: { keyword: input },
+        data: {
+          keyword:input,
+          user_ids:user_ids
+        },
         dataType: 'json'
       })
 
 
       .done(function(members) {
-        console.log(members)
+        // console.log(members)
         $("#user_search_result").empty();
         if (members.length !== 0) {
           members.forEach(function(user){
+
             appendUsers(user);
           })
         }
@@ -53,6 +61,7 @@ $(function() {
     $(document).on("click", '.user_search_add', function() {
       var name = $(this).attr("data-user-name");
       var user_id = $(this).attr("data-user-id");
+      user_ids.push(user_id)
       $(this).parent().remove();
       appendMembers(name, user_id);
     });
